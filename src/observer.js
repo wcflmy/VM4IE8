8cd5c40bb7a1f1185f13352c7091363206e0e573
afei <afei@mogujie.com>
2018-01-27 21:08:24 +0800

    reactive data for array

--- src/observer.js
+++ src/observer.js
@@ -1,19 +1,189 @@
+var arrayMethods = {}
+
+;[
+  'concat',
+  'copyWithin',
+  'entries',
+  'every',
+  'fill',
+  'filter',
+  'find',
+  'findIndex',
+  'forEach',
+  'includes',
+  'indexOf',
+  'join',
+  'keys',
+  'lastIndexOf',
+  'map',
+  'pop',
+  'push',
+  'reduce',
+  'reduceRight',
+  'reverse',
+  'shift',
+  'slice',
+  'some',
+  'sort',
+  'splice',
+  'toLocalString',
+  'toString',
+  'unshift'
+]
+  .forEach(function (method) {
+    // cache original method
+    var originalMethod = Array.prototype[method]
+    arrayMethods[method] = function mutator() {
+      // avoid leaking arguments:
+      // http://jsperf.com/closure-with-arguments
+      var i = arguments.length
+      var args = new Array(i)
+      while (i--) {
+        args[i] = arguments[i]
+      }
+      var originalLen = this.length
+      var ob = this.__ob__
+      var result = originalMethod.apply(ob.value, args)
+      ob.keys = Object.keys(ob.value)
+      var key, changed
+      var inserted = []
+      var removed = []
+      switch (method) {
+        case 'push':
+          for (key = originalLen, i = 0; key < originalLen + args.length; key++ , i++) {
+            this[key] = (Observer.create(args[i]) || {}).data || args[i]
+            inserted.push(this[key])
+          }
+          this.length = ob.value.length
+          changed = true
+          break
+        case 'unshift':
+          move(this, 0, args.length)
+          for (key = 0; key < args.length; key++) {
+            this[key] = (Observer.create(args[key]) || {}).data || args[key]
+            inserted.push(this[key])
+          }
+          this.length = originalLen + args.length
+          changed = true
+          break
+        case 'splice':
+          var index = args[0]
+          var deleteCount = args[1]
+          index = index > originalLen - 1 ? originalLen - 1 : index
+          index = index < 0 ? originalLen + index : index
+          deleteCount = deleteCount > originalLen - index ? originalLen - index : deleteCount
+          deleteCount = deleteCount < 0 ? 0 : deleteCount
+          args = args.slice(2)
+          var addedCount = args.length - deleteCount
+          for (key = index; key < index + deleteCount; key++) {
+            removed.push(this[key])
+            delete this[key]
+          }
+          move(this, index + deleteCount, addedCount)
+          // copy added item
+          for (i = 0; i < args.length; i++) {
+            this[index + i] = (Observer.create(args[i]) || {}).data || args[i]
+            inserted.push(this[index + i])
+          }
+          this.length = originalLen + addedCount
+          changed = true
+          break
+        case 'pop':
+          removed.push(this[originalLen - 1])
+          delete this[originalLen - 1]
+          this.length = originalLen - 1
+          changed = true
+          break
+        case 'shift':
+          removed.push(this[0])
+          for (key = 1; key < this.length; key++) {
+            this[key - 1] = this[key]
+          }
+          delete this[originalLen - 1]
+          this.length = originalLen - 1
+          changed = true
+          break
+        case 'sort':
+        case 'reverse':
+          var copy = []
+          for (key = 0; key < this.length; key++) {
+            copy[key] = this[key]
+          }
+          originalMethod.apply(copy, args)
+          for (key = 0; key < this.length; key++) {
+            this[key] = copy[key]
+          }
+          changed = true
+          break
+        default:
+          // result = originalMethod.apply(this, args)
+          break
+      }
+      if (changed) {
+        if (inserted) ob.observeArray(inserted)
+        if (removed) ob.unobserveArray(removed)
+        // notify change
+        ob.notify()
+      }
+      return result
+    }
+  })
+
+function move(array, index, offset) {
+  var key
+  var originalLen = array.length
+  if (offset > 0) {
+    // copy the items from low to high
+    for (key = originalLen - 1; key >= index; key--) {
+      array[key + offset] = array[key]
+    }
+  } else {
+    // copy the items from high to low
+    for (key = index; key < originalLen; key++) {
+      array[key + offset] = array[key]
+    }
+  }
+}
+
+Array.prototype.$set = function $set(index, val) {
+  if (index >= this.length) {
+    this.length = index + 1
+  }
+  return this.splice(index, 1, val)[0]
+}
+
+Array.prototype.$remove = function $remove(item) {
+  /* istanbul ignore if */
+  if (!this.length) return
+  var index = this.indexOf(item)
+  if (index > -1) {
+    return this.splice(index, 1)
+  }
+}
+
 function Observer(data) {
+  // store the original value
+  this.value = data
   var proxy = createDomProxy()
   var keys = Object.keys(data)
   var i = keys.length
   // copy data to the proxy
   while (i--) {
-    proxy[keys[i]] = JSON.parse(JSON.stringify(data[keys[i]]))
+    proxy[keys[i]] = data[keys[i]]
   }
   if(data.length) {     // for Array
     proxy.length = data.length
   }
   proxy.__ob__ = this
+  this.dep = new Dep()
   this.data = proxy
-  this.raw_data = data
   this.keys = keys
-  this.walk(proxy)
+  if(isArray(proxy)) {
+    copyAugment(proxy, arrayMethods)
+    this.observeArray(proxy)
+  } else {
+    this.walk(proxy)
+  }
 }
 
 Observer.create = function (value) {
@@ -40,6 +210,38 @@ Observer.prototype.walk = function (obj) {
   }
 }
 
+Observer.prototype.observeArray = function (items) {
+  var i = items.length
+  while (i--) {
+    var ob = Observer.create(items[i])
+    items[i] = (ob || {}).data || items[i]
+    if (ob) {
+      (ob.parents || (ob.parents = [])).push(this)
+    }
+  }
+}
+
+Observer.prototype.unobserveArray = function (items) {
+  var i = items.length
+  while (i--) {
+    var ob = items[i] && items[i].__ob__
+    if (ob) {
+      ob.parents.$remove(this)
+    }
+  }
+}
+
+Observer.prototype.notify = function () {
+  this.dep.notify()
+  var parents = this.parents
+  if (parents) {
+    var i = parents.length
+    while (i--) {
+      parents[i].notify()
+    }
+  }
+}
+
 Observer.prototype.convert = function (key, val) {
   this.defineReactive(this.data, key, val)
 }
@@ -57,6 +259,9 @@ var defineReactive = Observer.prototype.defineReactive = function (obj, key, val
       // console.log('get: ', key, Dep.target)
       if (Dep.target) {
         dep.depend()
+        if(childOb) {
+          childOb.dep.depend()
+        }
       }
       return val
     },
@@ -65,6 +270,7 @@ var defineReactive = Observer.prototype.defineReactive = function (obj, key, val
       if (newVal === val) {
         return
       }
+      obj.__ob__.value[key] = newVal          // update the original value
       childOb = Observer.create(newVal)
       val = (childOb || {}).data || newVal
       dep.notify()
@@ -86,6 +292,19 @@ function createDomProxy() {
       proxy[method] = Object.prototype[method]
     })
   return proxy
+}
+
+function isArray (array) {
+  if (!array) return false
+  var length = array.length
+  return length === 0 ||
+    typeof length === 'number' && length > 0 && (length - 1) in array
+}
+
+function copyAugment(target, src) {
+  for (var i in src) {
+    target[i] = src[i]
+  }
 }
 
 var uid = 0
