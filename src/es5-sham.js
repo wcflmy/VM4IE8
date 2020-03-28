2d441dc4bbd6f84cbdc2a18ffb982d93e808afc9
QJesus <208780931@qq.com>
2020-03-28 22:59:02 +0800

    move to es5-sham.js

--- src/es5-sham.js
+++ src/es5-sham.js
@@ -557,4 +557,26 @@
     };
   }
 
+  if (!Object.assign) {
+    Object.assign = function(target) {
+      'use strict';
+      if (target == null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+   
+      target = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source != null) {
+          for (var key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+              target[key] = source[key];
+            }
+          }
+        }
+      }
+      return target;
+    };
+  }
+
 }));
